6aaba61de71fb88caeb297009d73f8dfc59259eb
Mukesh Murugan <iammukeshm@gmail.com>
2020-07-25 13:55:23 +0530

    added efcore

---
+++ Datatables.ServerSide/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Datatables.ServerSide.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Datatables.ServerSide.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ Datatables.ServerSide/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Datatables.ServerSide.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Contact { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
